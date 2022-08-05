678b4ae9f8aa685f7ecf16a1679cdc6df4693ba4
duonglamh1999 <duonglamh1999@gmail.com>
2022-08-04 21:48:46 -0500

    first commit

---
+++ ap.py
@@ -0,0 +1,10 @@
+from logging import root
+from turtle import left, right
+
+
+class Tree():
+    def __init__(self):
+        self.root= root
+        self.left =left
+        self.right= right
+        self.value= value
